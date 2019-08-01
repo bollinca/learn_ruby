d595fd0e761fa83ea6b7f219bb8f38fa0128ea6d
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-01 14:20:28 -0400

    Solve 03

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,34 @@
-#write your code here
+def echo(value_in)
+    return value_in
+end
+
+def shout(value_in)
+    return value_in.upcase
+end
+
+def repeat(value_in, num = 2)
+    return ("#{value_in} " * num).strip
+end
+
+def start_of_word(str, length)
+    return str[0...length]
+end
+
+def first_word(str)
+    return str.split(' ')[0]
+end
+
+def titleize(str)
+    str = str.capitalize
+    str_arr = str.split(' ')
+    output = []
+    lowercase = ["and", "the", "over"]
+    
+    for word in str_arr
+        if !lowercase.include?(word)
+            word = word.capitalize
+        end
+        output.push(word)
+    end
+    output.join(' ')
+end
