c84f16038ce57e7f32015d4395fa13e45f9463c1
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-01 16:04:47 -0400

    Solve 04 without bonus

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,22 @@
-#write your code here
+def translate (words)
+    vowels = ["a", "e", "i", "o", "u"]
+    each_word = words.split(' ')
+    output = []
+
+    for word in each_word 
+        word_letters = word.split('')
+        
+        while !(vowels.include?(word_letters[0]))
+            if word_letters[0..1] == ["q", "u"]
+                word_letters = word_letters.push(word_letters.shift)
+            end
+            word_letters = word_letters.push(word_letters.shift)
+        end
+        
+        if vowels.include?(word_letters[0])
+            word = word_letters.join
+            output.push(word + 'ay')
+        end
+    end
+    return output.join(" ")
+end
