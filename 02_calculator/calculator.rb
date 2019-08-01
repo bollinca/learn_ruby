3672398b680898aa5373cda480223de774966ad2
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-01 13:30:21 -0400

    Solve 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,39 @@
-#write your code here
+def add(num1, num2)
+    return num1 + num2
+end
+
+def subtract(num1, num2)
+    return num1 - num2
+end
+
+def sum(nums)
+    sum = 0
+    nums.each do |num|
+        sum += num
+    end
+    return sum
+end
+
+def multiply(nums)
+    sum = 1
+    nums.each do |num|
+        sum *= num
+    end
+    return sum
+end
+
+def power(num1, num2)
+    return num1 ** num2
+end
+
+def factorial(num)
+    solution = num
+    if num == 0 || num == 1
+        solution = 1
+    end
+    while num > 1
+        solution *= (num - 1)
+        num = num - 1
+    end
+    return solution
+end
