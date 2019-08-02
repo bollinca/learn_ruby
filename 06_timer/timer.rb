e00d67524b5134529f1fef6eb44b1deb0bdcad6b
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-02 12:53:36 -0400

    Solve 06

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,29 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds, :minutes, :hours
+  attr_writer :minutes, :hours
+
+  def initialize 
+    @seconds = 0
+    @minutes = 0
+    @hours = 0
+  end
+
+  def seconds=(input)
+    while input >= 60
+      input = input - 60
+      @minutes = @minutes + 1
+      if @minutes >= 60
+        @minutes -= 60
+        @hours += 1
+      end
+    end
+    @seconds = input
+  end
+  
+  def time_string
+    @hour_disp = "%02d" % @hours
+    @min_disp = "%02d" % @minutes
+    @sec_disp = "%02d" % @seconds
+    return "#{@hour_disp}:#{@min_disp}:#{@sec_disp}"
+  end
 end
