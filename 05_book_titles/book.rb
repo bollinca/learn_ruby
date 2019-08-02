896ebe4305a212649b9cd9658c3c68b28d6182c2
bollinca <51376777+bollinca@users.noreply.github.com>
2019-08-02 12:05:48 -0400

    Solve 05

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,20 @@
 class Book
-# write your code here
+    attr_reader :title
+
+    def title=(title)
+        title = title.capitalize
+        title_split = title.split(' ')
+
+        lowercase = ["and", "in", "the", "of", "a", "an"]
+        new_title_split = []
+        for word in title_split
+            if !(lowercase.include?(word))
+                word_cap = word.capitalize
+                new_title_split.push(word_cap)
+            else 
+                new_title_split.push(word)
+            end
+        end
+        @title = new_title_split.join(' ')
+    end
 end
